23bb01a504a0e67b10e6a11a2c9b8c6d47743f60
Thierry1 <floveandroid@sina.com>
2020-06-18 00:06:40 +0800

    提交Day14

---
+++ Day14/代码/src/main/java/com/example/jgjz/fragment/ui/login/LoginViewModelFactory.java
@@ -0,0 +1,26 @@
+package com.example.jgjz.fragment.ui.login;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+
+import com.example.jgjz.fragment.data.LoginDataSource;
+import com.example.jgjz.fragment.data.LoginRepository;
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+public class LoginViewModelFactory implements ViewModelProvider.Factory {
+
+    @NonNull
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+            return (T) new LoginViewModel(LoginRepository.getInstance(new LoginDataSource()));
+        } else {
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
+}
